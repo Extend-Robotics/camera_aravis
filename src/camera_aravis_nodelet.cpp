e8ae8dbd03905665ec61acda7a1ef3aa3c67dab1
Bartosz Meglicki <bmegli@op.pl>
2023-07-14 13:37:01 +0200

    enable multipart output format only when configured for substreams

--- src/camera_aravis_nodelet.cpp
+++ src/camera_aravis_nodelet.cpp
@@ -454,8 +454,17 @@ void CameraAravisNodelet::onInit()
   if (use_ptp_stamp_)
     resetPtpClock();
 
-  // enable multipart data
-  aravis::camera::set_multipart_output_format(p_camera_, true);
+  // enable multipart data if we configure for substreams
+  // chunked data is not implemented yet so we use multipart
+  bool has_substreams = std::any_of(streams_.begin(), streams_.end(),
+                                   [](const Stream &stream)
+                                     { return stream.substreams.size() > 1; });
+
+  if(has_substreams)
+  {
+    ROS_INFO("Detected substreams - enabling multipart output");
+    aravis::camera::set_multipart_output_format(p_camera_, true);
+  }
 
   // spawn camera stream in thread, so onInit() is not blocked
   spawning_ = true;
